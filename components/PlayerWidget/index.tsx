815a41253b6965f7067a80db75eddd28f073f346
Deep <deep@Deeps-MacBook-Air.local>
2021-01-26 17:49:44 +0530

    AWS Amplify

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,23 +1,77 @@
-import React from "react";
-import { Text, Image, View } from 'react-native';
+import React, {useEffect, useState} from "react";
+import {Text, Image, View, TouchableOpacity} from 'react-native';
 import { AntDesign, FontAwesome} from "@expo/vector-icons";
-
+import { Audio } from 'expo-av';
 
 import styles from './styles';
 import {Song} from "../../types";
+import {Sound} from "expo-av/build/Audio/Sound";
 
 const song = {
     id: '1',
+    uri: 'https://not-just-trash.s3-eu-west-1.amazonaws.com/WhatsApp+Audio+2020-09-22+14.20.25.mp4',
     imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
     title: 'High on You',
     artist: 'Helen'
 }
 
 const PlayerWidget = () => {
+
+    const [sound, setSound] = useState<Sound|null>(null);
+    const [isPlaying, setIsPlaying] = useState<boolean>(true);
+    const [duration, setDuration] = useState<number|null>(null);
+    const [position, setPosition] = useState<number|null>(null);
+
+    const onPlaybackStatusUpdate = (status) => {
+        setIsPlaying(status.isPlaying);
+        setDuration(status.durationMillis);
+        setPosition(status.positionMillis);
+    }
+
+    const playCurrentSong = async () => {
+        if(sound) {
+            await sound.unloadAsync();
+        }
+
+        const { sound: newSound } = await Sound.createAsync(
+            { uri: song.uri },
+            { shouldPlay: isPlaying },
+            onPlaybackStatusUpdate
+        )
+
+        setSound(newSound)
+    }
+
+    useEffect(() => {
+        //play the song
+        playCurrentSong();
+    }, [])
+
+    const onPlayPausePress = async () => {
+        if (!sound) {
+            return;
+        }
+        if (isPlaying) {
+            await sound.stopAsync();
+        } else {
+            await sound.playAsync();
+        }
+    }
+
+    const getProgress = () => {
+        if (sound === null || duration === null || position === null) {
+            return 0;
+        }
+
+        return  (position / duration) * 100;
+    }
+
     return (
         <View style={styles.container}>
-            {/*Image cover*/}
-            <Image source={{ uri: song.imageUri }} style={styles.image} />
+            <View style={[styles.progress, { width: '${getProgress()}%'}]} />
+            <View style={styles.row}>
+                {/*Image cover*/}
+                <Image source={{ uri: song.imageUri }} style={styles.image} />
                 <View style={styles.rightContainer}>
                     <View style={styles.nameContainer}>
                         {/*Title*/}
@@ -31,11 +85,14 @@ const PlayerWidget = () => {
                         {/*Hear Button*/}
                         <AntDesign name="hearto" size={30} color={"white"}/>
 
+
                         {/*Play Button*/}
-                        <FontAwesome name="play" size={30} color={"white"}/>
+                        <TouchableOpacity onPress={onPlayPausePress()}>
+                            <FontAwesome name={isPlaying ? 'pause' : 'play'} size={30} color={"white"}/>
+                        </TouchableOpacity>
                     </View>
                 </View>
-
+            </View>
         </View>
     )
 }
